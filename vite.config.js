6565fd0be1ff65f25383ff9df9e3aa48a382fa19
Abner <contactfinance588@gmail.com>
2026-07-29 05:51:08 -0500

    Fix: configurar despliegue automatico en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/mi-rutina-en-una-app/',
   plugins: [react()],
   server: {
     host: '127.0.0.1',
